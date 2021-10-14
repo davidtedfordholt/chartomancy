7ca4b821b55f43ae361214ad487fab338f3cb63a
David Holt <d@tdfd.org>
2021-10-14 19:06:09 -0400

    add plot, data

--- chartomancy/server.R
+++ chartomancy/server.R
@@ -17,24 +17,45 @@ shinyServer(function(input, output) {
         x = as.Date(character()),
         y = numeric())
     
-    
-    output$distPlot <- renderPlot({
+    output$data_plot <- renderPlot({
+        past_to_plot <-
+            past %>%
+            filter(key == input$selected_key) %>%
+            select(x, y)
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        future_to_plot <-
+            past_to_plot %>%
+            filter(x == max(past_to_plot$x)) %>%
+            bind_rows(predictions$ts) %>%
+            as_tsibble()
+        
+        max_date <- as.Date(max(past_to_plot$x) + as.integer(input$horizon))
+                
+        ggplot() +
+            geom_line(aes(x, y), past_to_plot, color = 'dodgerblue') +
+            geom_point(aes(x, y), future_to_plot, color = 'orange', size = 2) +
+            geom_smooth(aes(x, y), future_to_plot, color = 'orange') +
+            xlim(as.Date(min(past_to_plot$x)), max_date)
 
     })
     
-    output$data_plot <- renderPlot({
-        predictions$ts %>%
-            as_tsibble() %>%
-            autoplot()
-            
-        
+    observeEvent(input$plot_click, {
+        new_point <- data.frame(x = as.Date(input$plot_click$x, origin = "1970-01-01"),
+                              y = input$plot_click$y)
+        if (new_point$x %in% predictions$ts$x) {
+            predictions$ts <- predictions$ts[-which(predictions$ts$x == new_point$x), ]
+        }
+        predictions$ts <- bind_rows(predictions$ts, new_point)
+    })
+    
+    ## 4. remove row on actionButton click ##
+    observeEvent(input$rem_point, {
+        predictions$ts[-nrow(predictions$ts), ] <- predictions$ts[-nrow(predictions$ts), ]
+    })
+    
+    ## 5. render a table of the growing dataframe ##
+    output$forecast_table <- renderTable({
+        predictions$ts
     })
 
 })
